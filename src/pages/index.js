4b5dd9440cbda326e595baa4922a4fe80606e8a4
xyj <593500664@qq.com>
2019-11-11 14:54:59 +0800

    add README

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function() {
     <div className={styles.normal}>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started,k edit <code>src/pages/index.js</code> and save to.</li>
+        <li>To get started,k dit <code>src/pages/index.js</code> and sa to.</li>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
             Getting Started
