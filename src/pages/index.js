3f6f99be5cac31f5c124dfd4dd17f21bdee4b47f
xyj <593500664@qq.com>
2019-11-11 10:56:03 +0800

    test git

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import styles from './index.css';
-
+import styles from './index.less';
 
 export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <li>To get started,k edit <code>src/pages/index.js</code> and save to.</li>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
             Getting Started
