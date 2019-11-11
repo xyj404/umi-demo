116ea149b5d3d3bcc62dda2b3992605ca7dc4e1a
xyj <593500664@qq.com>
2019-11-11 16:04:41 +0800

    umi-demo change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
+      umi-demo change
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
